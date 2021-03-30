e1104cf4af2b13fab9c5e18ee86e507c9a384bac
saamiukarim <saamiukarim1@gmail.com>
2021-03-30 23:33:40 +0600

    last

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import './App.css';
+import About from "./components/About/About";
+import Home from "./components/Home/Home";
+import PostDetail from "./components/PostDetail/PostDetail";
+
+function App() {
+  return (
+    <div>
+       <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/home">Home 2
+            </Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+        <Switch>
+          <Route exact path="/">
+            <Home/>
+          </Route>
+          <Route path="/about">
+            <About/>
+          </Route>
+          <Route path="/dashboard">
+          </Route>
+          <Route path="/home">
+            <Home/>
+          </Route>
+          <Route path="/post/:id">
+            <PostDetail></PostDetail>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/PostDetail/PostDetail.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import Comment from '../Comment/Comment';
+
+const PostDetail = () => {
+    const {id} = useParams();
+    const [post, setPost] = useState({});
+    const [ comments, setComments] = useState([]);
+    useEffect(() => {
+        const url = `https://jsonplaceholder.typicode.com/posts/${id}`;
+        fetch(url)
+        .then (res => res.json())
+        .then(data => setPost(data));
+    },[])
+    useEffect(()=> {
+        const url = `https://jsonplaceholder.typicode.com/comments?postid=${id}`;
+        fetch(url)
+        .then(res => res.json())
+        .then(data => setComments(data))
+    },[])
+    return (
+        <div>
+            <h3> This is :{id}</h3>
+            <p> User Posted:{post.id}</p>
+            <p>title:{post.title}</p>
+            <p>post body:{post.body}</p>p
+            <p>Number of comments: {comments.length}</p>
+            {
+                comments.map(comment => <Comment comment={comment}></Comment>)
+            }
+        </div>
+    );
+};
+
+export default PostDetail;
